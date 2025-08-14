1adeda1fde8d321de8bd07b691939a2d4a55c0dd
Noah Chamberland <noah.chamberland@gmail.com>
2025-08-13 23:07:36 -0400

    Fix system tests for friends functionality

--- test/system/friends_test.rb
+++ test/system/friends_test.rb
@@ -1,8 +1,12 @@
 require "application_system_test_case"
 
 class FriendsTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
+
   setup do
     @friend = friends(:one)
+    @user = users(:one)
+    sign_in @user
   end
 
   test "visiting the index" do
@@ -14,11 +18,11 @@ class FriendsTest < ApplicationSystemTestCase
     visit friends_url
     click_on "New friend"
 
-    fill_in "Email", with: @friend.email
-    fill_in "First name", with: @friend.first_name
-    fill_in "Last name", with: @friend.last_name
-    fill_in "Phone", with: @friend.phone
-    fill_in "Twitter", with: @friend.twitter
+    fill_in "Email", with: "new@example.com"
+    fill_in "First name", with: "New"
+    fill_in "Last name", with: "Friend"
+    fill_in "Phone", with: "555-1234"
+    fill_in "Twitter", with: "newuser"
     click_on "Create Friend"
 
     assert_text "Friend was successfully created"
@@ -29,11 +33,11 @@ class FriendsTest < ApplicationSystemTestCase
     visit friend_url(@friend)
     click_on "Edit this friend", match: :first
 
-    fill_in "Email", with: @friend.email
-    fill_in "First name", with: @friend.first_name
-    fill_in "Last name", with: @friend.last_name
-    fill_in "Phone", with: @friend.phone
-    fill_in "Twitter", with: @friend.twitter
+    fill_in "Email", with: "updated@example.com"
+    fill_in "First name", with: "Updated"
+    fill_in "Last name", with: "Name"
+    fill_in "Phone", with: "555-9999"
+    fill_in "Twitter", with: "updated"
     click_on "Update Friend"
 
     assert_text "Friend was successfully updated"
@@ -42,7 +46,7 @@ class FriendsTest < ApplicationSystemTestCase
 
   test "should destroy Friend" do
     visit friend_url(@friend)
-    click_on "Destroy this friend", match: :first
+    click_on "Delete", match: :first
 
     assert_text "Friend was successfully destroyed"
   end
